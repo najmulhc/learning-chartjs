20b9a150ac574f3b67c1a75db8d80a4c7f07927f
Najmul Huda Chowdhury Chayan <87802203+najmulhc@users.noreply.github.com>
2023-03-11 12:17:02 +0600

    base project for all charts is done

---
+++ src/component/Containers/ChartContainers.styled.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+//main container 
+export const MainChartContainer = styled.div``;
+
+// containers for charts 
+export const BarContainer = styled.div``;
+export const LineContainter = styled.div``;
+export const PieContainer = styled.div``;
+export const RadarContrainer = styled.div``;
+export const PolerAreaContainer = styled.div``;
